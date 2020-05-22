b0820a9acb1526e6338725f998c65d1c119ed928
Vicne <vicnevicne@gmail.com>
2020-05-22 02:19:59 +0200

    Moved TODO list

--- src/info/ginj/CaptureEditingFrame.java
+++ src/info/ginj/CaptureEditingFrame.java
@@ -1,8 +1,13 @@
 package info.ginj;
 
+import info.ginj.ui.GinjButton;
+import info.ginj.ui.GinjButtonBar;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class CaptureEditingFrame extends JFrame {
     private BufferedImage capturedImg;
@@ -11,6 +16,9 @@ public class CaptureEditingFrame extends JFrame {
         super();
         this.capturedImg = capturedImg;
         final Dimension capturedImgSize = new Dimension(capturedImg.getWidth(), capturedImg.getHeight());
+
+        final Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
 
         JPanel imagePanel = new JPanel() {
             @Override
@@ -43,7 +51,55 @@ public class CaptureEditingFrame extends JFrame {
 
         JScrollPane scrollableImagePanel = new JScrollPane(imagePanel);
 
-        getContentPane().add(scrollableImagePanel);
+        contentPane.add(scrollableImagePanel, BorderLayout.CENTER);
+
+        // Prepare button bar
+        JPanel actionPanel = new JPanel(); // To add a margin around buttonBar
+        actionPanel.setLayout(new FlowLayout(FlowLayout.LEADING, 0, 0));
+        actionPanel.setName("GinjPanel"); // To be used as a selector in laf.xml
+        JPanel buttonBar = new GinjButtonBar();
+        try {
+            GinjButton shareButton = new GinjButton("Share via X", new ImageIcon(ImageIO.read(getClass().getResource("img/b_share.png"))));
+            shareButton.addActionListener(e -> onExport("share"));
+            buttonBar.add(shareButton);
+            GinjButton saveButton = new GinjButton("Save", new ImageIcon(ImageIO.read(getClass().getResource("img/b_save.png"))));
+            saveButton.addActionListener(e -> onExport("disk"));
+            buttonBar.add(saveButton);
+            final JButton copyButton = new GinjButton("Copy", new ImageIcon(ImageIO.read(getClass().getResource("img/b_copy.png"))));
+            copyButton.addActionListener(e -> onExport("clipboard"));
+            buttonBar.add(copyButton);
+            final JButton cancelButton = new GinjButton("Cancel", new ImageIcon(ImageIO.read(getClass().getResource("img/b_cancel.png"))));
+            cancelButton.addActionListener(e -> onCancel());
+            buttonBar.add(cancelButton);
+            final JButton customizeButton = new GinjButton("Customize Ginj buttons", new ImageIcon(ImageIO.read(getClass().getResource("img/b_customize.png"))));
+            customizeButton.addActionListener(e -> onCustomize());
+            buttonBar.add(customizeButton);
+        }
+        catch (IOException e) {
+            System.out.println("Error loading capture button images");
+            e.printStackTrace();
+            System.exit(Ginj.ERR_STATUS_LOAD_IMG);
+        }
+        actionPanel.add(buttonBar);
+
+        contentPane.add(actionPanel, BorderLayout.SOUTH);
+
         pack();
+
+        // Center window
+        setLocationRelativeTo(null);
     }
+
+    private void onExport(String exportType) {
+
+    }
+
+    private void onCancel() {
+        dispose();
+    }
+
+    private void onCustomize() {
+
+    }
+
 }

58f6f2af5c3b4ea0e34208ba5ed0f86ef866832d
Vicne <vicnevicne@gmail.com>
2020-05-29 15:55:10 +0200

    Update TODO list

--- src/info/ginj/Ginj.java
+++ src/info/ginj/Ginj.java
@@ -2,16 +2,14 @@ package info.ginj;
 
 /*
 TODO Features :
- - Fix toolbar buttons L&F
-
  - Do overlays
  - Implement fixed ratio to 16:9 (shift-drag) or 4:3 (ctrl-drag) + snap to resp  640x360,800x450,960x540,1280x720 or 320x240,400x300,640x480,800x600,1024x768 / ENHANCEMENT: in 4:3 1280x960
  - Implement Window detection using JNA
  - Implement "exports"
- - Implement local storage
+ - Implement history
 
 TODO UI:
- - Finalize Look and feel
+ - Finalize Look and feel (title bar)
  - Fix scrollbar corner + thumb icon + colors + gap
  - Upscale sun and sun-rays so that runtime downscale provides an anti-aliasing, or better yet draw it by code (gradients etc)
  - Build 3 main buttons at runtime based on circle + icons (downscale provides an anti-aliasing)

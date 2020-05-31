6ede7fcec0c6f883e627e1bc11336144fba44e2b
Vicne <vicnevicne@gmail.com>
2020-06-01 01:17:41 +0200

    Implement resize of rectangle overlays

--- src/info/ginj/tool/RectangleOverlay.java
+++ src/info/ginj/tool/RectangleOverlay.java
@@ -20,15 +20,16 @@ public abstract class RectangleOverlay extends Overlay {
 
     /**
      * Returns all handles of the component
-     * By convention, when a component is first drawn, getHandles()[0] is the end of the drawing (arrowhead or second point of rectangle)
+     * By convention, handle index 0 is the release position when first drawing a component (arrow head or end of rectangle diagonal)
+     * We then turn clockwise from there
      * @return
      */
     @Override
     public Point[] getHandles() {
         return new Point[]{
-                new Point(rectangle.x - 3, rectangle.y - 3),
-                new Point(rectangle.x - 3, rectangle.y + rectangle.height + 3),
                 new Point(rectangle.x + rectangle.width + 3, rectangle.y + rectangle.height + 3),
+                new Point(rectangle.x - 3, rectangle.y + rectangle.height + 3),
+                new Point(rectangle.x - 3, rectangle.y - 3),
                 new Point(rectangle.x + rectangle.width + 3, rectangle.y - 3)
         };
     }
@@ -36,16 +37,30 @@ public abstract class RectangleOverlay extends Overlay {
     /**
      * This method indicates that the given handle has moved to a new position
      * By convention, handle index 0 is the release position when first drawing a component (arrow head or end of rectangle diagonal)
+     * We then turn clockwise from there
      * @param handleIndex
      * @param newPosition
      */
     @Override
     public void setHandlePosition(int handleIndex, Point newPosition) {
-        if (handleIndex == 0) {
-            Coords.setX2(rectangle, newPosition.x, 0);
-            Coords.setY2(rectangle, newPosition.y, 0);
+        switch (handleIndex) {
+            case 0 -> {
+                Coords.setX2(rectangle, newPosition.x, 0);
+                Coords.setY2(rectangle, newPosition.y, 0);
+            }
+            case 1 -> {
+                Coords.setX1(rectangle, newPosition.x, 0);
+                Coords.setY2(rectangle, newPosition.y, 0);
+            }
+            case 2 -> {
+                Coords.setX1(rectangle, newPosition.x, 0);
+                Coords.setY1(rectangle, newPosition.y, 0);
+            }
+            case 3 -> {
+                Coords.setX2(rectangle, newPosition.x, 0);
+                Coords.setY1(rectangle, newPosition.y, 0);
+            }
         }
-        // Do it
     }
 
     @Override

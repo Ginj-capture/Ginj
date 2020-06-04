a62cbed2c244b9a52c24e91578851c8146f35a37
Vicne <vicnevicne@gmail.com>
2020-06-04 23:53:33 +0200

    first implementation of actual Text area in TextOverlay (with dummy text)

--- src/info/ginj/tool/text/TextOverlay.java
+++ src/info/ginj/tool/text/TextOverlay.java
@@ -1,13 +1,41 @@
 package info.ginj.tool.text;
 
 import info.ginj.tool.RectangleOverlay;
+import info.ginj.ui.Util;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class TextOverlay extends RectangleOverlay {
+    Color textColor = Util.TEXT_TOOL_DEFAULT_FOREGROUND_COLOR;
 
-    // TODO there should be a JTextArea inside
-    // So should Overlay extend JPanel instead of JComponent ?
+    private JTextPane textPane;
+
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(Color textColor) {
+        this.textColor = textColor;
+    }
+
+
+    @Override
+    public TextOverlay initialize(Point initialPoint, Color initialColor) {
+        super.initialize(initialPoint, initialColor);
+        textPane = new JTextPane();
+        textPane.setBackground(Util.TEXTFIELD_BACKGROUND_COLOR);
+        textPane.setForeground(initialColor);
+        textPane.setSelectionColor(Util.TEXTFIELD_SELECTION_BACKGROUND_COLOR);
+        textPane.setFont(new Font("Arial", Font.PLAIN, 18));
+textPane.setText("ABCDEFGHIJKLMNO\n" +
+        "abcdefghijklmno\n" +
+        "1234567890\n" +
+        "&!§$ù%\n" +
+        "selected");
+        add(textPane);
+        return this;
+    }
 
     @Override
     public String getPresentationName() {
@@ -16,6 +44,10 @@ public class TextOverlay extends RectangleOverlay {
 
     @Override
     public void drawComponent(Graphics2D g2d, int xOffset, int yOffset) {
+        Rectangle textRectangle = new Rectangle(rectangle);
+        textRectangle.grow(-10, -8);
+        textPane.setBounds(textRectangle);
+        textPane.setForeground(getColor());
         g2d.setColor(Color.WHITE);
         g2d.fillRoundRect(rectangle.x+2 + xOffset, rectangle.y+2 + yOffset, rectangle.width-4, rectangle.height-4, 8, 8);
         g2d.setColor(getColor());
--- src/info/ginj/tool/text/TextTool.java
+++ src/info/ginj/tool/text/TextTool.java
@@ -2,10 +2,23 @@ package info.ginj.tool.text;
 
 import info.ginj.tool.GinjTool;
 import info.ginj.tool.Overlay;
+import info.ginj.ui.Util;
 
 import java.awt.*;
 
 public class TextTool implements GinjTool {
+    // Remember color for new text overlays
+    Color textColor = Util.TEXT_TOOL_DEFAULT_FOREGROUND_COLOR;
+
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    // TODO should be called upon overlay text color change
+    public void setTextColor(Color textColor) {
+        this.textColor = textColor;
+    }
+
     @Override
     public String getName() {
         return "Text";
@@ -13,6 +26,8 @@ public class TextTool implements GinjTool {
 
     @Override
     public Overlay createComponent(Point initalPosition, Color initialColor) {
-        return new TextOverlay().initialize(initalPosition, initialColor);
+        final TextOverlay overlay = new TextOverlay().initialize(initalPosition, initialColor);
+        overlay.setTextColor(getTextColor());
+        return overlay;
     }
 }

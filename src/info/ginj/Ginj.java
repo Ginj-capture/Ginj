51fcedf3887e003b202ce05287117dc433892c81
Vicne <vicnevicne@gmail.com>
2020-06-06 02:19:54 +0200

    Updated comments

--- src/info/ginj/Ginj.java
+++ src/info/ginj/Ginj.java
@@ -2,20 +2,20 @@ package info.ginj;
 
 /*
 TODO Features :
- - Implement Text overlay
- - Draw prettier handles
- - Fix rectangle resizing
+ - Fix rectangle resizing (when reducing past opposite side, rectangle slips)
  - Implement color chooser
  - Implement fixed ratio to 16:9 (shift-drag) or 4:3 (ctrl-drag) + snap to resp 640x360,800x450,960x540,1280x720 or 320x240,400x300,640x480,800x600,1024x768 / ENHANCEMENT: in 4:3 1280x960
  - Implement Windows detection using JNA
- - Implement "exports"
- - Implement history
+ - More work on "exports"
  - Implement video using ffmpeg
+ - Implement history
+ - Implement preferences
  - Should undo/redo change selection inside the Action methods (e.g change color, resize) ? - or completely deselect component after operation
+ - Add progress + notification when exporting copy/save (+ auto fade when mouse not over - checkbox) + Close button
 
 TODO UI:
  - Paint title bar
- - Finalize Look and feel (OK button in dialogs)
+ - Finalize Look and feel (OK button in dialogs, File chooser (save as))
  - Fix scrollbar corner + thumb icon + colors + gap
  - Upscale sun and sun-rays so that runtime downscale provides an anti-aliasing, or better yet draw it by code (gradients etc)
  - Build 3 main buttons at runtime based on circle + icons (downscale provides an anti-aliasing)

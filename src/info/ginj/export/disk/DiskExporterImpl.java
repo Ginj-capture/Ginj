6778b8d40c880d8a027b9e65638c005ecdd94677
Vicne <vicnevicne@gmail.com>
2020-06-12 15:42:14 +0200

    Take preferences into account when saving (+ attempt to speed up FileChooser)

--- src/info/ginj/export/disk/DiskExporterImpl.java
+++ src/info/ginj/export/disk/DiskExporterImpl.java
@@ -1,5 +1,7 @@
 package info.ginj.export.disk;
 
+import info.ginj.Ginj;
+import info.ginj.Prefs;
 import info.ginj.export.GinjExporter;
 
 import javax.imageio.ImageIO;
@@ -14,6 +16,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 /**
  * This exporter saves the image as a PNG file to disk, and copies its path to the clipboard
@@ -25,7 +28,7 @@ public class DiskExporterImpl extends GinjExporter implements ClipboardOwner {
     }
 
     /**
-     * Exports the given image
+     * Exports the given image to disk
      *
      * @param image          the image to export
      * @param exportSettings a set of properties that could contain exporter-specific parameters
@@ -33,36 +36,81 @@ public class DiskExporterImpl extends GinjExporter implements ClipboardOwner {
      */
     @Override
     public boolean export(BufferedImage image, Properties exportSettings) {
-        // TODO only prompt if required to do so in preferences, otherwise save in the default location
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle("Save capture as...");
-        fileChooser.setAcceptAllFileFilterUsed(false);
-        fileChooser.setMultiSelectionEnabled(false);
-        fileChooser.setFileFilter(new FileNameExtensionFilter("PNG (*.png)", "png"));
-        fileChooser.setSelectedFile(new File(exportSettings.getProperty("captureId") + ".png"));
-        if (fileChooser.showSaveDialog(getFrame()) == JFileChooser.APPROVE_OPTION) {
-            File file = fileChooser.getSelectedFile();
-            boolean confirm = true;
-            if (file.exists()) {
-                if (JOptionPane.showConfirmDialog(getFrame(), "Are you sure you want to overwrite\n" + file.getAbsolutePath() + "\n?", "File exists", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.NO_OPTION) {
-                    confirm = false;
-                }
-            }
-            if (confirm) {
-                try {
-                    ImageIO.write(image, "png", file);
-                    StringSelection stringSelection = new StringSelection(file.getAbsolutePath());
-                    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                    clipboard.setContents(stringSelection, this);
-                    return true;
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                    JOptionPane.showMessageDialog(getFrame(), "Encoutered an error while saving image as\n" + file.getAbsolutePath() + "\n" + e.getMessage() + "\nMore info is available on the Java console", "Save Error", JOptionPane.ERROR_MESSAGE);
+
+        // Determine where to save the file
+        boolean askForLocation = Prefs.isTrue(Prefs.Key.USE_CUSTOM_LOCATION);
+        String saveDirName;
+        if (askForLocation) {
+            // If a default location is set and valid, use it
+            saveDirName = Prefs.get(Prefs.Key.DEFAULT_CUSTOM_SAVE_LOCATION_DIR);
+            if (saveDirName == null || saveDirName.isBlank() || !new File(saveDirName).exists()) {
+                // Otherwise default to "last saved", if defined and valid
+                saveDirName = Prefs.get(Prefs.Key.LAST_CUSTOM_SAVE_LOCATION_DIR);
+                if (saveDirName == null || saveDirName.isBlank() || !new File(saveDirName).exists()) {
+                    // Otherwise default to the current dir
+                    saveDirName = new File("").getAbsolutePath();
                 }
             }
         }
-        return false;
+        else {
+            // If a save location is set and valid, use it
+            saveDirName = Prefs.get(Prefs.Key.SAVE_LOCATION_DIR);
+            if (saveDirName == null || saveDirName.isBlank() || !new File(saveDirName).exists()) {
+                // Otherwise default to the current dir, and prompt
+                saveDirName = new File("").getAbsolutePath();
+                askForLocation = false;
+            }
+        }
+        // Default file
+        File file = new File(saveDirName, exportSettings.getProperty("captureId") + ".png");
+
+        if (askForLocation) {
+            JFileChooser fileChooser = null;
+            // TODO does this bring real performance boost commpared to fileChooser = new JFileChooser(); ?
+            try {
+                fileChooser = Ginj.futureFileChooser.get();
+            }
+            catch (InterruptedException | ExecutionException e) {
+                JOptionPane.showMessageDialog(getFrame(), "Error opening file chooser: " + e.getMessage());
+                e.printStackTrace();
+            }
+            fileChooser.setDialogTitle("Save capture as...");
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            fileChooser.setMultiSelectionEnabled(false);
+            fileChooser.setFileFilter(new FileNameExtensionFilter("PNG (*.png)", "png"));
+            fileChooser.setSelectedFile(file);
+
+            if (fileChooser.showSaveDialog(getFrame()) == JFileChooser.APPROVE_OPTION) {
+                file = fileChooser.getSelectedFile();
+                if (file.exists()) {
+                    if (JOptionPane.showConfirmDialog(getFrame(), "Are you sure you want to overwrite: " + file.getAbsolutePath() + "\n?", "File exists", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.NO_OPTION) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        try {
+            ImageIO.write(image, "png", file);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(getFrame(), "Encoutered an error while saving image as\n" + file.getAbsolutePath() + "\n" + e.getMessage() + "\nMore info is available on the Java console", "Save Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (askForLocation) {
+            // Remember selected path
+            Prefs.set(Prefs.Key.LAST_CUSTOM_SAVE_LOCATION_DIR, file.getParent());
+            Prefs.save();
+        }
+
+        // Copy path to clipboard
+        StringSelection stringSelection = new StringSelection(file.getAbsolutePath());
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, this);
+
+        return true;
     }
 
     @Override

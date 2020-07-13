4b6fb9a604eff9245028395326dee7c8a196c7f0
Vicne <vicnevicne@gmail.com>
2020-07-13 02:34:44 +0200

    (comment)

--- src/main/java/info/ginj/ui/component/GinjBorderedPanel.java
+++ src/main/java/info/ginj/ui/component/GinjBorderedPanel.java
@@ -3,8 +3,8 @@ package info.ginj.ui.component;
 import javax.swing.*;
 import java.awt.*;
 
-// TODO for better-looking panels (rounded corners),
-// see the "Working with Custom Painters" section of https://www.ibm.com/developerworks/library/j-synth/index.html
+// TODO for better-looking panels (rounded corners), see the "Working with Custom Painters" section of https://www.ibm.com/developerworks/library/j-synth/index.html
+// See also http://www.jyloo.com/news/?pubId=1268844895000
 public class GinjBorderedPanel extends JPanel {
     public GinjBorderedPanel() {
         this(true);

aa2acb0aee67829d05d10156c25e7947234e07cd
Vicne <vicnevicne@gmail.com>
2020-07-13 22:05:01 +0200

    Optimized hit check

--- src/main/java/info/ginj/tool/highlight/HighlightOverlay.java
+++ src/main/java/info/ginj/tool/highlight/HighlightOverlay.java
@@ -11,6 +11,20 @@ public class HighlightOverlay extends RectangleOverlay {
         return "Highlight";
     }
 
+    /**
+     * Simplified version as highlights don't have a shadow
+     * @param point the point to test
+     * @return
+     */
+    @Override
+    public boolean containsPoint(Point point) {
+        // First see if we're in a handle
+        if (isSelected() && getHandleIndexAt(point) != NO_INDEX) return true;
+
+        // Otherwise, see if we're in the rectangle
+        return rectangle.contains(point);
+    }
+
     @Override
     public void drawComponent(Graphics2D g2d, int xOffset, int yOffset) {
         g2d.setColor(UI.getTranslucentColor(getColor()));
